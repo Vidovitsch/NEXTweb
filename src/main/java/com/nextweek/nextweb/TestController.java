3a3ac53dd893c738942cf54312dab54cce04d216
Bekker,David D.S.H. de <david.de.bekker@live.nl>
2017-03-06 09:22:33 +0100

    update commit

---
+++ src/main/java/com/nextweek/nextweb/TestController.java
@@ -0,0 +1,17 @@
+package com.nextweek.nextweb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author David
+ */
+@Controller
+public class TestController {
+    
+    @RequestMapping("/test")
+    public void method() {
+        
+    }
+}

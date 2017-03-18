f1e156803b9651557ed626ab8eeb4c56359e2406
ArnoDekkersLos <A_Dekkerslos@hotmail.com>
2017-03-18 17:39:45 +0100

    added an extra layer of inheritance, this way the code of EventDate can be used both for the Events(workshop and stuff) and for the regular school days. prevents duplicating code.

---
+++ src/main/java/Models/EventDay.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+/**
+ *
+ * @author Arno Dekkers Los
+ */
+public class EventDay extends EventDate {
+    public EventDay(String eventName){super(eventName);}
+}

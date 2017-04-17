07a67cc8c4ac888f77fa5d2fff1ed5ba00ed0eab
Janssen,Michael M.M. <michael.janssen@student.fontys.nl>
2017-04-18 01:34:23 +0200

    Mapcreation: - Added a mapcreation page + controller - Added the controller reference to the dispatcher-servlet
    Mapcreation relies heavily on the javascript files which are located in:
    - /javascript/map/..
    Also it has it's own css file located in:
    - /css/map.css
    And it's own image files located in:
    - images/map/..
    
    It contains the following javascript files:
    1. circle, line, rectangle: these are object blueprints (classes)
    2. mapcreation: this file takes care of the canvas input and holds the list of objects
    3. mapoptions: this file takes care of the menu options above the canvas.. 
    EG: 
    - Draw generates 4 options: create rectangle, circle, line or text
    - Resize allows the user to 'resize' the elements and shows the resizing changes
    - Modify allows the user to select an element and change it's properties on text basis. Also if nothing is selected the combobox can be used to create a new element by text input as well.
    - File allows the user to load a file or save a file. (however, not yet implemented, high priority)
    
    KNOWN BUGS:
    - Rectangle resizing: topleft corner resizing is not working as intended
    - Line resizing: the line becomes unclickable if resized vertically and also in some other not-straight positions. 
    
    TO DO (priority from most important to least important):
    MAPCREATION <for admins>:
    - Saving to database & loading from database
    - Fixing resizing bugs
    - Implementing text option
    - Adding an option to view all elements in a list, combobox or w/e so they can be fixed/removed if something goes wrong
    MAP <for regular viewers>:
    - Loading from database
    - Selecting a table and viewing a group (??) - might be very challenging to make this one work...

---
+++ src/main/webapp/javascript/map/circle.js
@@ -0,0 +1,96 @@
+/****************
+ * Circle class *
+ ****************/
+var circle = (function () {
+    // constructor
+    function circle(id, x, y, radius) {
+		this.id = id;
+        this.x = x;
+        this.y = y;
+		this.radius = radius;
+		this.type = "circle";
+		
+		this.strokeStyle = '#000000';
+		
+		this.borderSelected = false;
+				
+        this.redraw(this.x, this.y);
+        return (this);
+    }
+	// Redraw the rectangle - makes use of Draw
+    circle.prototype.redraw = function (x, y) {
+        this.x = x || this.x;
+        this.y = y || this.y;
+        this.draw();
+        return (this);
+    }
+    // 
+    circle.prototype.highlight = function (x, y) {
+        this.x = x || this.x;
+        this.y = y || this.y;
+        this.draw();
+        return (this);
+    }
+    // Draw the rectangle
+    circle.prototype.draw = function () {
+        ctx.save();
+        ctx.beginPath();
+		ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+		ctx.strokeStyle = this.strokeStyle;
+		ctx.lineWidth = 5;
+        ctx.stroke();
+        ctx.restore();
+    }
+	// Check if the click is close enough
+	circle.prototype.checkCloseEnough = function(mouseX, mouseY) {
+		// Check if the border is selected
+		var distanceX = mouseX - this.x;
+		var distanceY = mouseY - this.y;
+		
+		var root = Math.sqrt(Math.pow(distanceX, 2) + Math.pow(distanceY, 2));
+		//console.log("Show root: " + root);
+		//console.log("Radius: " + this.radius + " " + (this.radius - 5) + "& " + (root < this.radius));
+		//console.log("X: " + this.x + ", Y: " + this.y);
+		//console.log("MouseX: " + mouseX + ", MouseY: " + mouseY);
+		
+		if (root >= this.radius - 5 && root <= this.radius) {
+			this.borderSelected = true;
+		}
+		//console.log("border selected: " + this.borderSelected);
+	}
+	// Move the circle
+	circle.prototype.moveTo = function(mouseX, mouseY) {
+		this.x = mouseX; 
+		this.y = mouseY; 
+		
+		this.draw();
+		return (this);
+	}	
+    // Check if a point is inside the rectangle
+    circle.prototype.isPointInside = function (mouseX, mouseY) {
+		var bool = Math.pow(mouseX - this.x, 2) + Math.pow(mouseY - this.y, 2) < Math.pow(this.radius, 2);
+		//console.log(Math.pow(mouseX - this.x, 2) + Math.pow(mouseY - this.y, 2));
+		//console.log(Math.pow(this.radius, 2));
+		return bool;
+    }
+	// Resize the rectangle
+	circle.prototype.resizeTo = function(mouseX, mouseY) {
+		// Calculate the difference for the new radius
+		var distanceX = mouseX - this.x;
+		var distanceY = mouseY - this.y;
+		
+		this.radius = Math.sqrt(Math.pow(distanceX, 2) + Math.pow(distanceY, 2));
+		this.draw();
+		return (this);
+	}
+	// Set the resizing values on false again
+	circle.prototype.stopResize = function() {
+		this.borderSelected = false;
+	}
+	// Check if currently resizing the circle
+	circle.prototype.isResizing = function() {
+		if (this.borderSelected) return true;
+		else return false;
+	}
+    return circle;
+})();
